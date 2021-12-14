e1af1c053119ced79c3018161710e2881d8fd1ce
nichro02 <nichro02@gmail.com>
2021-12-13 20:21:15 -0700

    Debugged MongoDB connection, created folders for config, controllers, models

--- server/index.js
+++ server/index.js
@@ -1,24 +1,36 @@
 //configure server
-import express from 'express'
-import bodyPaser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const dbConfig = require('./config/db.config')
+const cors = require('cors')
 //instantiate app and general setup
-const app = express()
+const app= express()
+
+require('dotenv').config()
+
+require('./routes/blogPosts.routes')(app)
+
+
+//dotenv.config()
+
+
 //handle different sized images
 app.use(bodyParser.json({ limit: '50mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
-//set up Mongoose
-const DB_CONNECTION = process.env.DATABASE_URL
-const PORT = process.env.PORT || 6000
+app.use(cors())
 
-mongoose
-  .connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+//app.use("/api/blogs", blogPosts)
+
+//set up Mongoose
+const db = require('./models/index')
+const dbURL = process.env.MONGODB_URL || `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
+//const DB_CONNECTION = process.env.DATABASE_URL
+const PORT = process.env.PORT || 4000
+
+db.mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server is running @ : http://localhost:${PORT}`)
@@ -26,4 +38,18 @@ mongoose
   )
   .catch((error) => console.error(error))
 
-app.use(cors())
+//simple route, do I work?
+app.get('/', (req, res)=> {
+    res.json({message: "Welcome to the home page"})
+
+})
+
+//routes
+require('./routes/blogPosts.routes')(app)
+
+
+//set prot, listen for request
+// const PORT = process.env.PORT || 8080
+// app.listen(PORT, () => {
+//     console.log(`Server running on ${PORT}`)
+// })
---
+++ server/routes/blogPosts.routes.js
@@ -0,0 +1,12 @@
+const controller = require('../controllers/blogPosts.controller')
+
+module.exports = function(app) {
+    app.use((req,res, next)=> {
+        //set header and allow use of x access token (we will use this to pass our token)
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-type, Accept"
+        );
+        next();
+    })
+}
