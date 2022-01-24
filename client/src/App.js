156df3e4e3d5ac5053800f44dcd529681df1f72a
nichro02 <nichro02@gmail.com>
2022-01-23 18:55:10 -0700

    Created Home component to serve as homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { Routes, Route} from 'react-router-dom'
+import './App.css'
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import useStyles from './styles/app.styles'
+
+//import components
+import BlogPosts from './components/BlogPosts'
+import BlogPostsForm from './components/BlogPostsForm'
+import Home from './components/Home/Home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+    return(
+        <h1>Hello world from the home component</h1>
+        
+    )
+}
+
+export default Home
