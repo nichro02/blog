6efababb4b6eee145ef90ef312313a8abcf246a4
nichro02 <nichro02@gmail.com>
2022-01-26 18:50:52 -0700

    Wrote auth services to handle register,login, and logout

---
+++ client/services/auth.service.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { getItem, setItem, removeItem } from '../utilities/localStorage.utilities'
+
+const API_URL = 'http://localhost:4000/api/auth/'
+
+//register new user
+export const register = (username, email, password) => {
+    return axios
+    .post(API_URL+'register', {
+        username,
+        email,
+        password
+    })
+}
+//login existing user
+export const login = (username,password) => {
+    return axios
+    .post(API_URL+'login', {
+        username,
+        password
+    })
+    .then(response => {
+        if(response.data.accessToken){
+            setItem('user', response.data)
+        }
+        return response.data
+    })
+}
+//get current user
+export const getCurrentUser = () => {
+    return getItem('user')
+}
+//logout user
+export const logout = () => {
+    removeItem('user')
+}
--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import { useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
-//create auth services to get current user and handle logout
+
 
 const Layout = (props) => {
     //structure: container div->nav bar->props.children to wrap components in Layout
