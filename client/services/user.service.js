b3c4d098695fdb846f8672bff7735327e33d51e4
nichro02 <nichro02@gmail.com>
2022-01-30 18:53:28 -0700

    Wrote user services to get user profile, follow, and unfollow users

---
+++ client/services/user.service.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import authHeader from '../utilities/auth.utilities'
+
+const API_URL = 'http://localhost:4000/api/'
+
+//get user profile
+export const userProfile = (id) => {
+    return axios.get(API_URL + 'user/profile/' + id)
+}
+
+//follow user
+export const followUser = (
+    currentUser,
+    followedUser
+) => {
+    return axios.put(API_URL + 'user/follow', {
+        currentUser,
+        followedUser
+    })
+}
+
+//unfollow user
+export const unfollowUser = (
+    currentUser,
+    unfollowedUser
+) => {
+    return axios.put(API_URL + 'user/follow', {
+        currentUser,
+        unfollowedUser
+    })
+}
