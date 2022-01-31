a0917d6ca553e0bda3128e5338e3531175f91c7c
nichro02 <nichro02@gmail.com>
2022-01-30 19:00:11 -0700

    Created post service file and listed services to write

---
+++ client/services/post.service.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:4000/api/post/'
+
+//get all posts
+
+//get single post
+
+//create post
+
+//edit post
+
+//delete post
+
+//upvote post
+
+//downvote post
+
+//repost a post
