b768d5db60d18ca6d8366097546c42f33f35d594
nichro02 <nichro02@gmail.com>
2022-02-01 18:53:31 -0700

    set up structure for signup form

---
+++ client/src/components/FormGroup/FormGroup.js
@@ -0,0 +1,10 @@
+const FormGroup = ({children, text}) => {
+    return(
+        <div>
+            <label htmlFor={text}>
+                {text.slice(0,1).toUpperCase() + text.slice(1, text.length)}
+            </label>
+            {children}
+        </div>
+    )
+}
---
+++ client/src/components/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import { useState, useRef } from 'react'
+
+//import components
+import FormGroup from '../FormGroup/FormGroup'
+
+//import helpers
+import { register, login } from '../../../services/auth.service'
+
+const SignUp = (props) => {
+    return(
+        <h1>Sign-inForm goes here</h1>
+    )
+}
