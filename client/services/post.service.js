25da9b8df761736d77518c0fc0e42957b4a19fe0
nichro02 <nichro02@gmail.com>
2022-02-01 18:20:11 -0700

    Wrote first half of post services

--- client/services/post.service.js
+++ client/services/post.service.js
@@ -3,12 +3,40 @@ import axios from 'axios'
 const API_URL = 'http://localhost:4000/api/post/'
 
 //get all posts
+export const allPosts = () => {
+    return axios.get(API_URL + 'all')
+}
 
 //get single post
+export const onePost = (idx) => {
+    return axios.get(API_URL + idx)
+}
 
-//create post
+//create new post
+export const newPost = (
+    title,
+    body,
+    tags,
+    author
+) => {
+    return axios.post(API_URL + 'new', {
+        title,
+        body,
+        tags,
+        author
+    })
+}
 
 //edit post
+export const editPost = (
+    id,
+    body
+) => {
+    return axios.put(API_URL + 'edit', {
+        id,
+        body
+    })
+}
 
 //delete post
 
