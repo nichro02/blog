e70c6e5793242da300bc78fd340dfbd176b5f1ff
nichro02 <nichro02@gmail.com>
2022-02-06 11:59:26 -0700

    Wrote functions to handle singup form entry

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -66,15 +66,54 @@ const SignUp = (props) => {
     const [message, setMessage] = useState("")
 
     //store username onChange
-
+    const onChangeUsername = e => {
+        const username = e.target.value
+        setUsername(username)
+    }
 
     //store password onChange
-
+    const onChangePassword = e => {
+        const password = e.target.value
+        setPassword(password)
+    }
 
     //store email onChange
-
+    const onChangeEmail = e => {
+        const email = e.target.value
+        setEmail(email)
+    }
 
     //handle signup form submit
+    const handleSignup = e => {
+        //stop page refresh
+        e.preventDefault()
+
+        setMessage('')
+        setSuccessful(false)
+
+        //validate form fields
+        form.current.validateAll()
+
+        //check to see if errors exist
+        if(checkButton.current.context._errors.length === 0){
+            register(username, email, password)
+            .then(response => {
+                setMessage(response.data.message)
+                setSuccessful(true)
+
+                login(username, password)
+                .then(() => {
+                    props.history.push('//home')
+                    window.location.reload()
+                })
+            },
+            (error) => {
+                setMessage(error)
+                setSuccessful(false)
+            }
+            )
+        }
+    }
     
     return(
         <h1>Sign-inForm goes here</h1>
