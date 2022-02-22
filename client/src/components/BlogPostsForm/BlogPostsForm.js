fd62d55a662c75beaaf9f35c678d00273247e68f
nichro02 <nichro02@gmail.com>
2022-02-21 19:26:39 -0700

    Wrote functions to handle form entry and submission for new posts

--- client/src/components/BlogPostsForm/BlogPostsForm.js
+++ client/src/components/BlogPostsForm/BlogPostsForm.js
@@ -4,6 +4,28 @@ import { newPost } from '../../services/post.service'
 
 const BlogPostForm = (props) => {
 
+    //manage state
+    const [post, setPost] = useState("")
+    
+    const currentUser = getCurrentUser()
+
+    //track post form changes
+    const onChange = e => {
+        const content = e.target.value
+        setPost(content)
+    }
+
+    //handle post submission
+    const handleNewPost = e => {
+        const tags = []
+        
+        //push tags to tags array
+        let tagArray = post.split(" ")
+        tagArray.forEach(word => {
+            tags.push(word)
+        })
+    }
+
     return(
         <div>
             New Blog Post Form
