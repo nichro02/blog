4567b532bd49725a59bda3efe54c831a36d3a5f1
nichro02 <nichro02@gmail.com>
2022-03-13 19:07:56 -0600

    Updated Reply to manage state and  handle form entry. Next, need to test component is working properly

--- client/src/components/Reply/Reply.js
+++ client/src/components/Reply/Reply.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { reply } from '../../services/post.service'
 
 import { getCurrentUser } from '../../services/auth.service'
 
@@ -9,11 +10,31 @@ const Reply = (props) => {
     const { from } = location.state
     console.log(from)
     console.log(from.title)
+    //manage state
     const [post, setPost] = useState('')
     //track post form changes
     const onChange = e => {
         const content = e.target.value
         setPost(content)
+    }
+    //process reply
+    const handleReply = e => {
+        const originalAuthor = from.author[0]._id
+        const originalPost = from.id
+        const tags = []
+        //push tags to tags array
+        let tagArray = post.split(" ")
+        tagArray.forEach(word => {
+            if(word.charAt(0) === '#') {
+                tags.push(word)
+            }
+        })
+        reply(
+            post,
+            tags,
+            originalAuthor,
+            originalPost
+        )
     }
     return(
         <div>
@@ -30,7 +51,7 @@ const Reply = (props) => {
             {currentUser && (
                 <div>
                 <h3>Your Reply</h3>
-                <form>
+                <form onSubmit={handleReply}>
                     <textarea
                             value={post}
                             onChange={onChange}
