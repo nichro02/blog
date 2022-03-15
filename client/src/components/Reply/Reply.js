a9ad5f48763baf3f8f4b1578e00d97adbfdd0d68
nichro02 <nichro02@gmail.com>
2022-03-14 20:56:59 -0600

    Troubleshooting Reply - will need to refactor component to pull in URL param to resolve location.state issue

--- client/src/components/Reply/Reply.js
+++ client/src/components/Reply/Reply.js
@@ -1,17 +1,29 @@
 import { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation,useNavigate } from 'react-router-dom'
 import { reply } from '../../services/post.service'
-
+import { useParams } from 'react-router'
 import { getCurrentUser } from '../../services/auth.service'
+
 
 const Reply = (props) => {
     const currentUser = getCurrentUser()
     const location = useLocation()
+    const navigate = useNavigate()
+    const { id } = useParams()
+    console.log(id)
     const { from } = location.state
+    //const originalPost = from._id
     console.log(from)
-    console.log(from.title)
+    // console.log(from.title)
     //manage state
     const [post, setPost] = useState('')
+    const [originalAuthor, setOriginalAuthor] = useState('')
+    const [originalPost,setOriginalPost] = useState('')
+    const [test, setTest] = useState({})
+    
+    // need to use URL param to preserve reply information
+    
+    
     //track post form changes
     const onChange = e => {
         const content = e.target.value
@@ -19,8 +31,8 @@ const Reply = (props) => {
     }
     //process reply
     const handleReply = e => {
-        const originalAuthor = from.author[0]._id
-        const originalPost = from.id
+        // const originalAuthor = from.author[0]._id
+        // const originalPost = from.id
         const tags = []
         //push tags to tags array
         let tagArray = post.split(" ")
@@ -35,7 +47,13 @@ const Reply = (props) => {
             originalAuthor,
             originalPost
         )
+        // navigate(`/post/${from.id}`)
     }
+
+    // const returnToPost = () => {
+    //     navigate(`/post/${from.id}`)
+    // }
+
     return(
         <div>
             <div>
