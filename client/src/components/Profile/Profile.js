ee32a6065acc9aa6873bd58382b483c0c814da61
nichro02 <nichro02@gmail.com>
2022-04-03 15:15:55 -0600

    Started building out Profile component

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import { getCurrentUser } from '../../services/auth.service'
+import { userProfile } from '../../services/user.service.service'
+
+const Profile = (props) => {
+
+
+    return(
+        <h1>User Profile goes here</h1>
+    )
+}
+
+export default Profile
