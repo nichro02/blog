cb78018ad066f831c45f77efdf90d6e0490ec993
nichro02 <nichro02@gmail.com>
2022-04-05 18:12:38 -0600

    useEffect to retrieve user information from database when page renders

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,10 +1,23 @@
 import { useState, useEffect } from 'react'
 import { getCurrentUser } from '../../services/auth.service'
 import { userProfile } from '../../services/user.service'
+import { useParams } from 'react-router'
 
 const Profile = (props) => {
 
+    const { id } = useParams()
+    console.log(id)
+    const getUserProfile = () => {
+        userProfile(id)
+        .then(response => {
+            console.log(response)
+        })
+    }
 
+    useEffect(() => {
+        getUserProfile()
+    }, [])
+    
     return(
         <h1>User Profile goes here</h1>
     )
