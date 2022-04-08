ad234388af373535a52213a65eebebb2149ca69a
nichro02 <nichro02@gmail.com>
2022-04-08 15:36:55 -0600

    Stored user data on profile on Profile component in a variable so  I can better access user info when structuring component

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -4,22 +4,31 @@ import { userProfile } from '../../services/user.service'
 import { useParams } from 'react-router'
 
 const Profile = (props) => {
-
+    const [userData, setUserData] = useState(null)
     const { id } = useParams()
     console.log(id)
+    
+    //retrieve user info
     const getUserProfile = () => {
         userProfile(id)
         .then(response => {
             console.log(response)
+            setUserData(response.data)
+            console.log(userData)
         })
     }
 
     useEffect(() => {
         getUserProfile()
     }, [])
-    
+
     return(
-        <h1>User Profile goes here</h1>
+        <div>
+            <h1>{userData.username}</h1>
+            <h3>Total posts: {userData.posts.length}</h3>
+            <h3>Upvotes received: {userData.upvote}</h3>
+        </div>
+        
     )
 }
 
